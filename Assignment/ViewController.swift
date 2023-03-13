19b4e319ba9ba0245e17f58a9f1e482a247a2a20
zagiko <m.sagik@gmail.com>
2023-03-13 17:56:26 +0200

    Initial Commit

---
+++ Assignment/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Assignment
+//
+//  Created by Mike on 13.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print("hello ")
+    }
+
+
+}
+
